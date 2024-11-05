88fabe6e7a4830c02c1da48afedf382c3b642b47
izarofserrano <i.serrano@opendeusto.es>
2024-11-05 13:36:15 +0100

    Cambios en el códgio

--- UDVogue/UDVogueApp/views.py
+++ UDVogue/UDVogueApp/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from .models import Editorial
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
+
 def index(request):
     return HttpResponse('primera vista')
+
+def listaEditoriales(request):
+    editoriales = Editorial.objects.order_by('nombre')
+    nombre_editoriales = ', '.join([editorial.nombre for editorial in editoriales])
+    return HttpResponse(nombre_editoriales)
+
+def detalleEDITORIAL(request, id_editorial):
+    try:
+        editorial = Editorial.objects.get(pk=id_editorial) 
+        revistas = editorial.revistas.all()
+
+        cadenaTxt = f"{editorial.nombre} - Contacto: {editorial.contacto}\n"
+
+        if revistas.exists():
+            cadenaTxt += "Revistas:\n"
+            for revista in revistas:
+                cadenaTxt += f"- {revista.titulo}, Fecha Publicación: {revista.fecha_publi}, Nº Edición: {revista.numero_edicion}\n"
+        else:
+            cadenaTxt += "No hay revistas asociadas a esta editorial."
+            
+        return HttpResponse(cadenaTxt)
+
+    except Editorial.DoesNotExist:
+        return HttpResponseNotFound("Editorial no encontrada")
