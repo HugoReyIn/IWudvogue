f46e79b49b2d9110b91b8e36d775d53302d86e39
Génesis Balcazar <g.balcazar@opendeusto.es>
2024-11-08 22:03:10 +0100

    añadimos vistas

--- UDVogue/UDVogueApp/views.py
+++ UDVogue/UDVogueApp/views.py
@@ -36,3 +36,25 @@ def listaProductos(request):
     productos = Producto.objects.order_by('nombre_producto')
     nombre_productos = ', '.join([producto.nombre_producto for producto in productos])
     return HttpResponse(nombre_productos)
+
+def detalleProducto(request, id_producto):
+    try:
+        producto = Producto.objects.get(pk=id_producto)
+        cadenaTxt = f"Producto: {producto.nombre_producto}\nDescripción: {producto.descripcion}\nPrecio: {producto.precio}\n"
+        return HttpResponse(cadenaTxt)
+    except Producto.DoesNotExist:
+        return HttpResponseNotFound("Producto no encontrado")
+
+def listaRevistas(request):
+    revistas = Revista.objects.order_by('titulo')
+    nombre_revistas = ', '.join([revista.titulo for revista in revistas])
+    return HttpResponse(nombre_revistas)
+
+def detalleRevista(request, id_revista):
+    try:
+        revista = Revista.objects.get(pk=id_revista)
+        editorial = revista.editorial
+        cadenaTxt = f"Revista: {revista.titulo}\nFecha de Publicación: {revista.fecha_publi}\nNúmero de Edición: {revista.numero_edicion}\nEditorial: {editorial.nombre}\n"
+        return HttpResponse(cadenaTxt)
+    except Revista.DoesNotExist:
+        return HttpResponseNotFound("Revista no encontrada")
