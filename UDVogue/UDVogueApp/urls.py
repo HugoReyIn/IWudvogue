e5c11b46fa9d4502c7265d40ade049a710727589
HugoReyIn <hugo.rey@opendeusto.es>
2024-11-09 21:24:36 +0100

    mejoras en el documenta

--- UDVogue/UDVogueApp/urls.py
+++ UDVogue/UDVogueApp/urls.py
@@ -3,53 +3,23 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('inicio' views.inicio, name='index'),
 
-    path('listadoDeEditoriales/', views.listadoEditoriales, name='lEditoriales'),
+    path('editoriales/', views.listaEditoriales, name = 'listaEditoriales'),
+    path('editoriales/<int:id_editorial>', views.detalleEditorial, name = 'detalleEditorial'),
+    
+    path('revistas/', views.listaRevistas, name = 'listaRevistas'),
+    path('revistas/<int:id_revista>', views.detalleRevista, name = 'detalleRevista'),
 
-    path('listadoDeRevistas/', views.listadoRevistas, name='lRevistas'),
+    path('productos/', views.listaProductos, name = 'listaProductos'),
+    path('productos/<int:id_producto', views.detalleProducto, name = 'detalleProducto'),
 
-    path('listadoDeProductos/', views.listadoProductos, name='lProductos'), 
+    path('editorialesHtml/', views.listaEditorialHtml, name = 'listaEditorialHtml'),
+    path('editorialesHtml/<int:id_editorial>', views.detalleEditorialHtml, name = 'detalleEditorialHtml'),
 
-    path('editorial/<int:id_editorial>', views.listaDetalleEditorial, name='detalleEditorial'),
+    path('revistasHtml/', views.listaRevistasHtml, name = 'listaRevistasHtml'),
+    path('revistasHtml/<int:id_revista>', views.detalleRevistaHtml, name = 'detalleRevistaHtml'),
 
-    path('revista/<int:id_revista>', views.listaDetalleRevista, name='detalleRevista'),
-
-    path('producto/<int:id_producto>', views.listadoDetallesProdcto, name='detalleProducto')
-]
-
-
-urlpatterns = [
-¡
-
-    #### VER MODELOS AHORA SOLO VIENEN LAS LISTAS
-
-    # /empresaAPP/listadoDeEmpresas/ - Lista de empresas en texto
-    path('listadoDeEmpresas/', views.listaEmpresas, name='listaE'),
-
-    # /empresaAPP/listadoDeTrabajadores/ - Lista de trabajadores en texto
-    path('listadoDeTrabajadores/', views.listaTrabajadores, name='listat'),
-
-    ### AHORA VIENE EL VER CADA UNO DE LOS MODELOS EN BASE DE DATOS
-
-    # /empresaAPP/empresas/5/ - Detalle de la empresa en texto
-    path('empresas/<int:id_empresa>', views.detalleEMPRESA, name='detalle'),
-
-    # /empresaAPP/detalleTrabajador/5/ - Detalle de trabajador en texto
-    path('detalleTrabajador/<int:id_trabajador>', views.detalleTrabajador, name='detalleTrabajador'),
-
-    ### VISTAS QUE USAN PLANTILLAS
-
-    # /empresaAPP/listadoDeTrabajadoresConPlantillas/
-    path('listadoDeTrabajadoresConPlantillas/', views.listaTrabajadores2, name='listaTrabajadoresPlantillas'),
-
-    # /empresaAPP/detalleTrabajadorConPlantillas/5/
-    path('detalleTrabajadorConPlantillas/<int:id_trabajador>', views.detalleTrabajadorConPlantillas, name='detalleTrabajadorPlantillas'),
-
-    # /empresaAPP/listadoDeEmpresasConPlantillas/
-    path('listadoDeEmpresasConPlantillas/', views.listaEmpresasConPlantillas, name='listaEmpresasPlantillas'),
-
-    # /empresaAPP/detalleEmpresaConPlantillas/5/
-    path('detalleEmpresaConPlantillas/<int:id_empresa>', views.detalleEmpresaConPlantillas, name='detalleEmpresaPlantillas'),
+    path('productosHtml/', views.listaProductosHtml, name = 'listaProductosHtml'),
+    path('productosHtml/<int:id_producto>', views.detalleProductoHtml, name = 'detalleProductoHtml'),
 ]
